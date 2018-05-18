f44d17f089ef1d480d223fdcacc4fe5ae09d79b0
AaronW <footballgrip89@yahoo.com>
2018-05-18 03:23:04 +0000

    Route authentication

---
+++ routes/index.js
@@ -0,0 +1,50 @@
+var express = require("express"),
+    router  = express.Router(),
+    Post    = require("../models/post"),
+    passport = require("passport"),
+    User    = require("../models/user");
+
+router.get("/", function(req, res){
+    Post.find({}, function(err, allPosts){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("posts/index", {posts: allPosts});
+        }
+    });
+});
+
+router.get("/register", function(req, res){
+   res.render("register");
+});
+
+router.post("/register", function(req, res){
+   var newUser = new User({username: req.body.username});
+   User.register(newUser, req.body.password, function(err, user){
+       if(err){
+           return res.render("register");
+       }
+       passport.authenticate("local")(req, res, function(){
+          res.redirect("/"); 
+       });
+   });
+});
+
+router.get("/login", function(req, res){
+    res.render("login");
+});
+
+router.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/",
+        failureRedirect: "/login"
+    }), function(req, res){
+});
+
+router.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+module.exports = router;

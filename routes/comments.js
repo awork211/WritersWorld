ed979fc6a86e87efdb5837453408168f4349e449
AaronW <footballgrip89@yahoo.com>
2018-05-18 03:20:16 +0000

    Route comments

---
+++ routes/comments.js
@@ -0,0 +1,42 @@
+var express = require("express"),
+    router  = express.Router(),
+    Post    = require("../models/post"),
+    Comment = require("../models/comment");
+
+router.get("/post/:id/comments/new", isLoggedIn, function(req, res){
+     Post.findById(req.params.id, function(err, foundPost){
+         if(err){
+             console.log(err);
+         } else {
+             res.render("comments/new", {post: foundPost});
+         }
+     });
+});
+
+router.post("/post/:id/comments", isLoggedIn, function(req, res){
+    Post.findById(req.params.id, function(err, foundPost){
+        if(err){
+            console.log(err);
+            res.redirect("/");
+        } else {
+            Comment.create(req.body.comment, function(err, comment){
+               if(err){
+                   console.log(err);
+               } else {
+                   foundPost.comments.push(comment);
+                   foundPost.save();
+                   res.redirect("/post/" + foundPost._id);
+               }
+            });
+        }
+    });
+});
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;

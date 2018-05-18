cc40c84d3eb1f2c4238e188ac8bb08e16b0fbe9e
AaronW <footballgrip89@yahoo.com>
2018-05-18 03:17:23 +0000

    Add user association

--- models/post.js
+++ models/post.js
@@ -4,6 +4,13 @@ var postSchema = new mongoose.Schema({
     name: String,
     image: String,
     content: String,
+    author: {
+         id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+         },
+         username: String
+   },
     comments: [
         {
          type: mongoose.Schema.Types.ObjectId,

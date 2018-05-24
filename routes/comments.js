ebb023ef6bd75435963692c750cc7f455885b2b9
AaronW <footballgrip89@yahoo.com>
2018-05-24 02:00:48 +0000

    Add routes and authorization

--- routes/comments.js
+++ routes/comments.js
@@ -1,9 +1,10 @@
 var express = require("express"),
     router  = express.Router(),
     Post    = require("../models/post"),
-    Comment = require("../models/comment");
+    Comment = require("../models/comment"),
+    middleware = require("../middleware");
 
-router.get("/post/:id/comments/new", isLoggedIn, function(req, res){
+router.get("/post/:id/comments/new", middleware.isLoggedIn, function(req, res){
      Post.findById(req.params.id, function(err, foundPost){
          if(err){
              console.log(err);
@@ -13,7 +14,7 @@ router.get("/post/:id/comments/new", isLoggedIn, function(req, res){
      });
 });
 
-router.post("/post/:id/comments", isLoggedIn, function(req, res){
+router.post("/post/:id/comments", middleware.isLoggedIn, function(req, res){
     Post.findById(req.params.id, function(err, foundPost){
         if(err){
             console.log(err);
@@ -36,11 +37,34 @@ router.post("/post/:id/comments", isLoggedIn, function(req, res){
     });
 });
 
-function isLoggedIn(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    res.redirect("/login");
-}
+router.get("/post/:id/comments/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+        if(err){
+            res.redirect("back");
+        } else {
+            res.render("comments/edit", {comment: foundComment, post_id: req.params.id});
+        }
+    });
+});
+
+router.put("/post/:id/comments/:comment_id", middleware.checkCommentOwnership, function(req, res){
+   Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updateComment){
+       if(err){
+           res.redirect("back");
+       } else {
+           res.redirect("/post/" + req.params.id);
+       }
+   });
+});
+
+router.delete("/post/:id/comments/:comment_id", middleware.checkCommentOwnership, function(req, res){
+   Comment.findByIdAndRemove(req.params.comment_id, function(err){
+       if(err){
+           res.redirect("back");
+       } else {
+           res.redirect("/post/" + req.params.id);
+       }
+   });
+});
 
 module.exports = router;

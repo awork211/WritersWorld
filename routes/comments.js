9698de1caff6a961cd06a31e3fd898276c9357ba
AaronW <footballgrip89@yahoo.com>
2018-05-27 23:09:49 +0000

    Updated err handling

--- routes/comments.js
+++ routes/comments.js
@@ -17,12 +17,13 @@ router.get("/post/:id/comments/new", middleware.isLoggedIn, function(req, res){
 router.post("/post/:id/comments", middleware.isLoggedIn, function(req, res){
     Post.findById(req.params.id, function(err, foundPost){
         if(err){
-            console.log(err);
+            req.flash("error", "Post not found.");
             res.redirect("/");
         } else {
             Comment.create(req.body.comment, function(err, comment){
                if(err){
-                   console.log(err);
+                   req.flash("error", "Something went wrong.");
+                   res.redirect("/");
                } else {
                    // add id and username
                    comment.author.id = req.user._id;
@@ -30,6 +31,7 @@ router.post("/post/:id/comments", middleware.isLoggedIn, function(req, res){
                    comment.save();
                    foundPost.comments.push(comment);
                    foundPost.save();
+                   req.flash("success", "Comment added!");
                    res.redirect("/post/" + foundPost._id);
                }
             });
@@ -40,6 +42,7 @@ router.post("/post/:id/comments", middleware.isLoggedIn, function(req, res){
 router.get("/post/:id/comments/:comment_id/edit", middleware.checkCommentOwnership, function(req, res){
     Comment.findById(req.params.comment_id, function(err, foundComment){
         if(err){
+            req.flash("error", "Comment not found.");
             res.redirect("back");
         } else {
             res.render("comments/edit", {comment: foundComment, post_id: req.params.id});
@@ -50,8 +53,10 @@ router.get("/post/:id/comments/:comment_id/edit", middleware.checkCommentOwnersh
 router.put("/post/:id/comments/:comment_id", middleware.checkCommentOwnership, function(req, res){
    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updateComment){
        if(err){
+           req.flash("error", "Post not found.");
            res.redirect("back");
        } else {
+           req.flash("success", "Comment was updated!");
            res.redirect("/post/" + req.params.id);
        }
    });
@@ -60,8 +65,10 @@ router.put("/post/:id/comments/:comment_id", middleware.checkCommentOwnership, f
 router.delete("/post/:id/comments/:comment_id", middleware.checkCommentOwnership, function(req, res){
    Comment.findByIdAndRemove(req.params.comment_id, function(err){
        if(err){
+          req.flash("error", "Something went wrong.");
            res.redirect("back");
        } else {
+           req.flash("success", "Comment was removed!");
            res.redirect("/post/" + req.params.id);
        }
    });
